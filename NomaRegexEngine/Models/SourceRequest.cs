91711e81e30efbcebc0a94e87c1b47e615a14a1c
Kostmika <mika.y.kost@gmail.com>
2025-07-21 02:35:30 +0300

    init

---
+++ NomaRegexEngine/Models/SourceRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NomaRegexEngine.Models
+{
+    public class SourceRequest
+    {
+        [Required]
+        public string Source { get; set; } = string.Empty;
+
+        [Required]
+        public string RegexPattern { get; set; } = string.Empty;
+    }
+}
---
+++ NomaRegexEngine/Program.cs
@@ -0,0 +1,28 @@
+using NomaRegexEngine.Services;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<RegexService>();
+builder.Services.AddHttpClient();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
